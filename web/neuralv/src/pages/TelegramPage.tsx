18c84f5c35072cacfa64be9303008e1da5a8bbd4
Perdonus <kala5ixa@gmail.com>
2026-03-29 03:20:36 +0300

    Polish NeuralV plugin report actions

--- web/neuralv/src/pages/TelegramPage.tsx
+++ web/neuralv/src/pages/TelegramPage.tsx
@@ -2,7 +2,7 @@ import { CenteredHeroSection } from '../components/CenteredHeroSection';
 import { StoryScene } from '../components/StoryScene';
 import '../styles/story.css';
 
-const telegramAssetVersion = '3.1.20260329.10';
+const telegramAssetVersion = '3.1.20260329.11';
 
 const telegramArtifacts = [
   {
